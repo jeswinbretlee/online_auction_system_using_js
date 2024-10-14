3a394bca0d1547d21e70299bd85358b7ed5382f9
jeswinbretlee <pauljeswing@gmail.com>
2024-10-14 22:02:39 +0530

    Delete client directory

--- client/src/actions/imageActions.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const addImage = imageData => async dispatch => {
-  const res = await axios.post('/api/listings/upload/image', imageData);
-  console.log(res);
-};
--- client/src/actions/notification.js
+++
@@ -1,9 +0,0 @@
-import { ADD_NOTIFICATION } from './types';
-
-export function addNotification(message, level) {
-  return {
-    type: ADD_NOTIFICATION,
-    message,
-    level
-  };
-}
